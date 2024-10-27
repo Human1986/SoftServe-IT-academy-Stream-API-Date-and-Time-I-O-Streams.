582c898c940feb1677b439e0f4f26f94c349bd4b
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-10-27 09:32:57 +0100

    Done

--- src/main/java/jom/com/softserve/s6/task2/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task2/MyUtils.java
@@ -2,11 +2,15 @@ package jom.com.softserve.s6.task2;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 
 public class MyUtils {
     public static String getDateAfterToday(int years, int months, int days) {
-        //Write code there
-        return "";
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+
+        LocalDate dateAfterToday = today.plusYears(years).plusMonths(months).plusDays(days);
+
+        return dateAfterToday.format(formatter);
+
     }
 }
