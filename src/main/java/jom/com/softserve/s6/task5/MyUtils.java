6a940806daf39b5b2cb07e3f4802565f6aa84a9a
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-10-28 21:43:03 +0100

    Done

--- src/main/java/jom/com/softserve/s6/task5/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task5/MyUtils.java
@@ -2,11 +2,21 @@ package jom.com.softserve.s6.task5;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class MyUtils {
 
-    //Write your code here
+    public Stream<String> nameList(Map<String, Stream<String>> map) {
+
+        return map.values().stream()
+                .flatMap(stream -> stream)
+                .filter(Objects::nonNull)
+                .map(s -> s.replace(" ", ""))
+                .filter(participant -> ! participant.isEmpty())
+                .map(participant -> participant.substring(0, 1).toUpperCase() + participant.substring(1).toLowerCase())
+                .distinct()
+                .sorted();
+
+    }
 
 }
