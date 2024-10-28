82a3bb6a12cef4876fa62aacd6fb35a03a951505
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-10-28 09:33:27 +0100

    Done

--- src/main/java/jom/com/softserve/s6/task3/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task3/MyUtils.java
@@ -1,11 +1,27 @@
 package jom.com.softserve.s6.task3;
 
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 
 public class MyUtils {
+    public static void writeFile(String filename, String text) {
+        StringBuilder line = new StringBuilder();
 
-    //Write your code here
+        for (char c : text.toCharArray()) {
+            StringBuilder binaryString = new StringBuilder(Integer.toBinaryString(c));
+            while (binaryString.length() < 7) {
+                binaryString.insert(0, "0");
+            }
+            line.append(binaryString);
+        }
 
+        try (FileWriter writer = new FileWriter((filename))) {
+            writer.write(line.toString());
+        } catch (IOException e) {
+            e.getCause();
+        }
+    }
 }
+
+
+
