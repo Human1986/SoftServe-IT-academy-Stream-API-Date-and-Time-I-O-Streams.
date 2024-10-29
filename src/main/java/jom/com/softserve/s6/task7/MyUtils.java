74a8d599349e148e5d9d977179ad84fdef3331b8
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-10-29 11:27:38 +0100

    Done

--- src/main/java/jom/com/softserve/s6/task7/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task7/MyUtils.java
@@ -1,13 +1,22 @@
 package jom.com.softserve.s6.task7;
 
-import java.util.Collection;
 import java.util.Objects;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MyUtils {
 
-    //Write your code here
+    public Stream<Integer> duplicateElements(Stream<Integer> stream) {
+
+     return  stream
+                .filter(Objects::nonNull)
+                .collect(Collectors.groupingBy(
+                        number -> number, Collectors.counting()))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() > 1)
+                .flatMap(entry -> Stream.generate(entry::getKey).limit(1))
+                .sorted();
+
+    }
 
 }
